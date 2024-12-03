2ac909a3f98f702b3e62ae14a74540a5265b3e16
GaGaGaGala <kosachevagalina@example.com>
2024-12-04 00:59:41 +0300

    Initial commit

---
+++ board/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from .forms import SignUpForm
+from django.contrib.auth import login
+from .models import Advertisement
+from .forms import AdvertisementForm
+
+# Create your views here.
+def base(request):
+    return render(request, 'base.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/board')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+@login_required
+def add_advertisement(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = form.save(commit=False)
+            advertisement.author = request.user
+            advertisement.save()
+            return redirect('board:advertisement_detail')
+    else:
+        form = AdvertisementForm()
+    return render(request, 'board/add_advertisement.html', {'form': form})
+def advertisement_detail(request, pk):
+    advertisement = Advertisement.objects.get(pk=pk)
+    return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
+def advertisement_list(request):
+    advertisements = Advertisement.objects.all()
+
+    return render(request, 'board/advertisement_list.html', {'advertisements': advertisements})
+
+
+
+
+
+
