dbf993dd3f4925b103983e9e82962ae77831d092
GaGaGaGala <kosachevagalina@example.com>
2024-12-07 23:53:21 +0300

    Обновлены файлы. Добавлены фотографии работы приложения.

--- board/views.py
+++ board/views.py
@@ -7,15 +7,18 @@ from .models import Advertisement
 from .forms import AdvertisementForm
 
 
-
-# Create your views here.
 def base(request):
+    """Представление для базового шаблона."""
     return render(request, 'base.html')
 
+
 def home(request):
+    """Представление для шаблона главной страницы."""
     return render(request, 'home.html')
 
+
 def signup(request):
+    """Представление для регистрации."""
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
@@ -26,11 +29,16 @@ def signup(request):
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
+
 def logout_view(request):
+    """Выход из приложения."""
     logout(request)
     return redirect('home')
+
+
 @login_required
 def add_advertisement(request):
+    """Для добавления объявления."""
     if request.method == "POST":
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
@@ -40,18 +48,24 @@ def add_advertisement(request):
     else:
         form = AdvertisementForm()
     return render(request, 'board/add_advertisement.html', {'form': form})
+
+
 def advertisement_detail(request, pk):
+    """Представление для страницы конкретного объявления."""
     advertisement = Advertisement.objects.get(pk=pk)
     return render(request, 'board/advertisement_detail.html', {'advertisement': advertisement})
-def advertisement_list(request):
-    advertisements = Advertisement.objects.all()
 
+
+def advertisement_list(request):
+    """Все объявления."""
+    advertisements = Advertisement.objects.all()
     return render(request, 'board/advertisement_list.html', {'advertisements': advertisements})
+
 
 @login_required
 def edit_advertisement(request, pk):
-    #advertisement = Advertisement.objects.get(pk=pk)
-    print('Редактирую')
+    """Представление  для редактирования объявлений, которое загружает форму существующего объявления и сохраняет изменения.
+    """
     advertisement = Advertisement.objects.get(pk=pk)
     if request.method == "POST":
         form = AdvertisementForm(request.POST, request.FILES, instance=advertisement)
@@ -59,7 +73,6 @@ def edit_advertisement(request, pk):
             form.instance.author = request.user
             form.save()
             return redirect('board:advertisement_detail', pk=advertisement.pk)
-            #return redirect('board:advertisement_list')
     else:
         form = AdvertisementForm(instance=advertisement)
     return render(request, 'board/edit_advertisement.html', {'form': form, 'advertisement': advertisement})
